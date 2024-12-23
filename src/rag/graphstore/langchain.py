037e8890c84872553805adb37193b1a1ca3f8a93
Nico Döbele <nico.doebele@googlemail.com>
2024-12-23 16:16:18 +0100

    split llamaindex from langchain in knowledge graph files

--- src/rag/graphstore/langchain.py
+++ src/rag/graphstore/langchain.py
@@ -3,15 +3,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_ollama import ChatOllama
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 from langchain_neo4j import Neo4jGraph, GraphCypherQAChain
-from llama_index.core.retrievers import KnowledgeGraphRAGRetriever
-from llama_index.core import get_response_synthesizer, StorageContext
-from llama_index.core.query_engine import RetrieverQueryEngine
-from llama_index.llms.ollama import Ollama
-from llama_index.llms.openai import OpenAI as IndexOpenAI
-from llama_index.graph_stores.neo4j import Neo4jGraphStore
-import os
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
+import os
 
 def mock_load_text_to_graphdb(file_path: str) -> None:
     # load and preprocess text data
@@ -37,28 +31,6 @@ def mock_load_text_to_graphdb(file_path: str) -> None:
     graph_store.add_graph_documents(graph_documents)
 
 def ask_question_to_graphdb(question: str) -> str:
-    graph_store = Neo4jGraphStore(
-        url="bolt://localhost:7687", 
-        username="neo4j", 
-        password="password",
-    )
-
-    llm = Ollama(model="llama3.2")
-    #load_dotenv()
-    #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    #llm = IndexOpenAI(api_key=OPENAI_API_KEY)
-
-    storage_context = StorageContext.from_defaults(graph_store=graph_store)
-    graph_rag_retriever = KnowledgeGraphRAGRetriever(llm=llm, storage_context=storage_context, verbose=True)
-    query_engine = RetrieverQueryEngine.from_args(llm=llm, retriever=graph_rag_retriever)
-    retrieved_context = query_engine.query(question)
-
-    response_synthesizer = get_response_synthesizer(llm=llm, response_mode="compact")
-    response = response_synthesizer.synthesize(question, retrieved_context.source_nodes)
-
-    return (retrieved_context, response)
-
-def ask_question_to_graphdb_OLD(question: str) -> str:
     graph_store = Neo4jGraph(
         url="bolt://localhost:7687", 
         username="neo4j", 
