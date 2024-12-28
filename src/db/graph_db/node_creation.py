bbcd3bd9dfe61b2ff56e732cabcd20ec78745ef2
Nick Berndt <nick.berndt@student.reutlingen-university.de>
2024-12-28 16:48:31 +0100

    Populate meta-data node with further information

--- src/db/graph_db/node_creation.py
+++ src/db/graph_db/node_creation.py
@@ -1,31 +1,33 @@
-
-
-
-# Knoten mit dynamischen Daten erstellen 
-#from neo4j import GraphDatabase 
-# pip install neo4j 
-#from src.data_processing.data_pipeline import extract_meta_data
-#Verbindung zur neo4j Datenbank erstellen 
-
-#uri = "bolt://localhost:7474"
-#user = "neo4j"
-#password = "this_pw_is_a_test25218###1119jj"
-
-#driver = GraphDatabase.driver(uri, auth=(user, password))
-
 def create_meta_data_node(tx, meta_data):
     query = """
     CREATE (m:MetaData {
-        id: $meta_id, title: $title, view_count: $view_count,
-        uploader: $uploader
+        url_id: $url_id, 
+        title: $title, 
+        description: $description,
+        tags: $tags,
+        categories: $categories, 
+        upload_date: $upload_date, 
+        duration: $duration, 
+        uploader_url: $uploader_url,  
+        uploader: $uploader, 
+        thumbnail: $thumbnail,
+        view_count: $view_count,
+        like_count: $like_count,
+        age_limit: $age_limit
     })
     """
-    tx.run(query, meta_id=meta_data["id"], 
+    tx.run(query, url_id=meta_data["id"], 
            title=meta_data["title"], 
-           view_count=meta_data["view_count"],  
-           uploader=meta_data["uploader"]) 
-    
-#with driver.session() as session:
-#session.execute_write(create_meta_data_node, meta_data)
-          
+           description=meta_data["description"],
+           tags=meta_data["tags"],  
+           categories=meta_data["categories"], 
+           upload_date=meta_data["upload_date"],
+           duration=meta_data["duration"], 
+           uploader_url=meta_data["uploader_url"], 
+           uploader=meta_data["uploader"], 
+           thumbnail=meta_data["thumbnail"], 
+           view_count=meta_data["view_count"], 
+           like_count=meta_data["like_count"], 
+           age_limit=meta_data["age_limit"])  
+
 
