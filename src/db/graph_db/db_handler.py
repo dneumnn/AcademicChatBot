2d633f13ff9eee0e055901cc70b59d8c1596f976
Thimo Fröhner <thimo.froehner@student.reutlingen-university.de>
2024-12-28 15:57:40 +0100

    Add graph database handler

---
+++ src/db/graph_db/db_handler.py
@@ -0,0 +1,14 @@
+from neo4j import GraphDatabase
+from src.db.graph_db.data_insert import create_meta_data_node
+
+class GraphHandler:
+
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+
+    def close(self):
+        self.driver.close()
+
+    def create_meta_data_session(self, meta_data):
+        with self.driver.session() as session:
+            session.execute_write(create_meta_data_node, meta_data)
