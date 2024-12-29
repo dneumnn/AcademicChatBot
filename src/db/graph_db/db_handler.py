47025386316daff55a98f47e2a85aedb25c52d79
Thimo Fröhner <thimo.froehner@student.reutlingen-university.de>
2024-12-29 14:34:56 +0100

    Add function for execute write transaction to db for transcript nodes

--- src/db/graph_db/db_handler.py
+++ src/db/graph_db/db_handler.py
@@ -1,5 +1,6 @@
 from neo4j import GraphDatabase
 from src.db.graph_db.node_creation import create_meta_data_node
+from src.db.graph_db.node_creation import create_transcript_chunk_node
 
 class GraphHandler:
 
@@ -12,3 +13,7 @@ class GraphHandler:
     def create_meta_data_session(self, meta_data):
         with self.driver.session() as session:
             session.execute_write(create_meta_data_node, meta_data)
+
+    def create_transcript_chunk_session(self, chunk):
+        with self.driver.session() as session:
+            session.execute_write(create_transcript_chunk_node, chunk)
