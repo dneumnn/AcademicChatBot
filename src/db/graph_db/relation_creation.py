1f8f64da61bc3ada18ac1b85e4fd3b845f95e110
Thimo Fröhner <thimo.froehner@student.reutlingen-university.de>
2025-01-02 17:00:33 +0100

    Adjust create_next_relationship function

--- src/db/graph_db/relation_creation.py
+++ src/db/graph_db/relation_creation.py
@@ -1,26 +1,13 @@
 # File for the node relations 
 # Linear relationships: NEXT and PREVIOUS model the sequence and make navigation easier
 
-def create_next_relationship(tx, chunk_id1, chunk_id2):
+def create_next_relationship(tx, node_id1, node_id2):
     query = """
-    MATCH (c1:TranscriptChunk {id: $chunk_id1})
-    MATCH (c2:TranscriptChunk {id: $chunk_id2})
+    MATCH (c1:TranscriptChunk {node_id: $chunk_id1})
+    MATCH (c2:TranscriptChunk {node_id: $chunk_id2})
     CREATE (c1)-[:NEXT]->(c2)
     """
-    tx.run(query, chunk_id1=chunk_id1, chunk_id2=chunk_id2)
+    tx.run(query, chunk_id1=node_id1, chunk_id2=node_id2)
 
-# Liste von Chunks
-chunks = [
-    {'chunk_id': 'chunk_001', 'text': 'Hallo, wie geht es dir?', 'embedding': [0.1, 0.2, 0.3]},
-    {'chunk_id': 'chunk_002', 'text': 'Mir geht es gut, danke!', 'embedding': [0.2, 0.3, 0.4]},
-    {'chunk_id': 'chunk_003', 'text': 'Was machst du heute?', 'embedding': [0.1, 0.1, 0.2]},
-]
-"""""
-with driver.session() as session:
-    for i in range(len(chunks) - 1):
-        session.write_transaction(
-            create_next_relationship,
-            chunks[i]['chunk_id'],
-            chunks[i + 1]['chunk_id']
-        )
-"""
+
+
