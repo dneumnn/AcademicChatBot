0efbf3a1318384ef86600c9569c4e5c4fa43df28
Lukas <laib.lukas@googlemail.com>
2025-01-21 18:08:17 +0100

    Add chunk_max_length parameter to detailed

--- src/data_processing/data_pipeline.py
+++ src/data_processing/data_pipeline.py
@@ -211,7 +211,7 @@ def download_pipeline_youtube(url: str, chunk_max_length: int=550, chunk_overlap
                 chunked_text = add_chunk_overlap(merged_sentence, chunk_overlap_length)
             else:
                 detailed_llm_chunks = create_chunk_llm(processed_text_transcript)
-                check_detailed_llm_chunks = check_llm_chucks(detailed_llm_chunks, 1000)
+                check_detailed_llm_chunks = check_llm_chucks(detailed_llm_chunks, chunk_max_length)
                 format_detailed_llm_chunks = format_llm_chunks(check_detailed_llm_chunks)
                 chunked_text = add_chunk_overlap(format_detailed_llm_chunks, chunk_overlap_length)
             append_meta_data(meta_data, video_id, chunked_text)
